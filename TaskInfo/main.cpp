c762193431025a2c1e16844c6cfa6ea3c4f31269
Bayarma <bz12@ya.ru>
2018-09-05 12:31:24 +0300

    Комментарий кириллический

--- TaskInfo/main.cpp
+++ TaskInfo/main.cpp
@@ -17,11 +17,11 @@ enum class TaskStatus {
 using TasksInfo = map<TaskStatus, int>;
 
 class TeamTasks {
-    map <string, TasksInfo> Persons;
+     map <string, TasksInfo> Persons;
 public:
   // Получить статистику по статусам задач конкретного разработчика
   const TasksInfo& GetPersonTasksInfo(const string& person) const {
-
+     return Persons.at(person);
   };
 
   // Добавить новую задачу (в статусе NEW) для конкретного разработчитка
@@ -32,12 +32,35 @@ public:
   // Обновить статусы по данному количеству задач конкретного разработчика,
   // подробности см. ниже
   tuple<TasksInfo, TasksInfo> PerformPersonTasks(
-      const string& person, int task_count);
+      const string& person, int task_count) {
+      //1) приведем task_count к количеству невыполненных задач
+      //количество невыполненных задач
+      int undone_tasks = Persons[person][TaskStatus::NEW] +
+                            Persons[person][TaskStatus::IN_PROGRESS] +
+                                Persons[person][TaskStatus::TESTING];
+      if (task_count > undone_tasks) { task_count = undone_tasks;}
+      //переведем новые задачи
+      if (Persons[person][TaskStatus::NEW]<task_count) {
+            // переводим задачи в IN_PROGRESS
+            Persons[person][TaskStatus::IN_PROGRESS] = Persons[person][TaskStatus::IN_PROGRESS]+
+                                                                Persons[person][TaskStatus::NEW];
+            //обнуляем новые задачи
+            Persons[person][TaskStatus::NEW] = 0;
+            //меняем task_count
+            task_count = task_count - Persons[person][TaskStatus::NEW];}
+      };
 };
+void PrintTasksInfo(TasksInfo tasks_info) {
+  cout << tasks_info[TaskStatus::NEW] << " new tasks" <<
+      ", " << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress" <<
+      ", " << tasks_info[TaskStatus::TESTING] << " tasks are being tested" <<
+      ", " << tasks_info[TaskStatus::DONE] << " tasks are done" << endl;
+}
 int main()
 {
     TeamTasks Team;
     Team.AddNewTask("Petya");
+    PrintTasksInfo(Team.GetPersonTasksInfo("Petya"));
 
     return 0;
 }
